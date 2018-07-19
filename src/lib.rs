f6bcd0b79025b14a97651e180b47148e1122723d
Jesse Bees <jesse@toomanybees.com>
2018-07-19 00:15:06 -0400

    Add vendor c api for reference, plus openni2 types

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 // bindgen ./Include/NiteCAPI.h -o ~/code/rust/nite2-sys.rs --whitelist-function nite.* --rustified-enum Nite.* --opaque-type Oni.* -- -x c++ -I../OpenNI-MacOSX-x64-2.2/Include
 
 extern crate openni2_sys;
-use openni2_sys::{OniFrame, OniGeneralCallback};
+use openni2_sys::{OniDeviceHandle, OniFrame, OniGeneralCallback};
 
 use std::os::raw::{c_short, c_int, c_ulonglong, c_void};
 #[cfg(test)] use std::mem::{size_of, align_of};
@@ -790,7 +790,7 @@ extern "C" {
 }
 extern "C" {
     // FIXME: arg1 = deviceHandle?
-    pub fn niteInitializeUserTrackerByDevice(arg1: *mut c_void,
+    pub fn niteInitializeUserTrackerByDevice(arg1: OniDeviceHandle,
                                              arg2: *mut NiteUserTrackerHandle)
                                              -> NiteStatus;
 }
@@ -855,7 +855,7 @@ extern "C" {
     pub fn niteInitializeHandTracker(arg1: *mut NiteHandTrackerHandle) -> NiteStatus;
 }
 extern "C" {
-    pub fn niteInitializeHandTrackerByDevice(arg1: *mut c_void, // FIXME
+    pub fn niteInitializeHandTrackerByDevice(arg1: OniDeviceHandle,
                                              arg2: *mut NiteHandTrackerHandle)
                                              -> NiteStatus;
 }
